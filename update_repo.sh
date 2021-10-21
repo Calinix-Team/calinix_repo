0c4674f2310f871f43fdbfa16e777d50c4881c8b
Arghya <arghyasarkar.nolan@gmail.com>
2021-10-21 14:49:11 +0530

    Added chaotic

---
+++ update_repo.sh
@@ -0,0 +1,22 @@
+rm calinix*
+
+echo "repo-add"
+repo-add -s -n -R calinix.db.tar.gz *.pkg.tar.zst
+
+sleep 1
+
+rm calinix.db
+rm calinix.db.sig
+
+rm calinix.files
+rm calinix.files.sig
+
+mv calinix.db.tar.gz calinix.db
+mv calinix.db.tar.gz.sig calinix.db.sig
+
+mv calinix.files.tar.gz calinix.files
+mv calinix.files.tar.gz.sig calinix.files.sig
+
+echo "####################################"
+echo "Repo Updated!!"
+echo "####################################"
